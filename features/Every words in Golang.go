967c3acf12772bed79230cc21133698896497625
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-04 15:55:43 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -804,10 +804,26 @@ Output:
     6 57 9
     7 53 5
 
-    
+Enumerating the Bytes
+------------------------------------------------------------------------------------------------        
+example:
+    var price = "€48.95"
+    for index, char := range []byte(price) {
+        fmt.Println(index, char)
+    }
+Output:
+    0 226
+    1 130
+    2 172
+    3 52
+    4 56
+    5 46
+    6 57
+    7 53
 
-
-
+70.func
+    Functions are groups of code statements that are executed only when the function is
+    invoked during the flow of execution.
 
 
 
