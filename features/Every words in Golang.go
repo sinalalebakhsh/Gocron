78ad3792dc1d27226bc9a3d7bc0920738fb03777
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-04 14:33:53 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -694,7 +694,26 @@ Understanding Flow Control
             fmt.Println("Key:", key, "Value:", value)
         }
 
-
+    Enumerating a Map in Order:
+        import (
+                "fmt"
+                "sort"
+            )
+        func main() {
+            products := map[string]float64 {
+                "Kayak" : 279,
+                "Lifejacket": 48.95,
+                "Hat": 0,
+            }
+            keys := make([]string, 0, len(products))
+            for key, _ := range products {
+                keys = append(keys, key)
+            }
+            sort.Strings(keys)
+            for _, key := range keys {
+                fmt.Println("Key:", key, "Value:", products[key])
+            }
+        }
 
 
 
