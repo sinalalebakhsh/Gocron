a2b62f542eef4a1d9ddbb5b2d05bab0b07849a3b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-04 05:50:58 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -258,19 +258,24 @@ Operator    Description
 
             
 Converting Floating-Point Values to Integers
-------------------------------------------------------------------------------------------------
 Functions in the math Package for Converting Numeric Types
-Function            Description
---------            -------------------------------------------------------------
-Ceil(value)         This function returns the smallest integer that is greater than the specified floating-
-                    point value. The smallest integer that is greater than 27.1, for example, is 28.
+------------------------------------------------------------------------------------------------
+39.Ceil(value)
+    This function returns the smallest integer that is greater than the specified floating-
+    point value. The smallest integer that is greater than 27.1, for example, is 28.
 
-Floor(value)        This function returns the largest integer that is less than the specified floating-point
-                    value. The largest integer that is less than 27.1, for example, is 28.
+40.Floor(value)
+    This function returns the largest integer that is less than the specified floating-point
+    value. The largest integer that is less than 27.1, for example, is 28.
 
 Round(value)        This function rounds the specified floating-point value to the nearest integer.
 
 RoundToEven(value)  This function rounds the specified floating-point value to the nearest even integer.
+
+
+
+Parsing from Strings
+
 
 
     package main = first executable file main.go
