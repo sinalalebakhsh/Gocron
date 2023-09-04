dccf4bc5727a6cedb8b016cee154b4bb5c3540de
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-04 16:43:37 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -878,8 +878,18 @@ Output:
         return price + (price * 0.2)
     }
 
+75.Returning Multiple Function Results
+    func swapValues(first, second int) (int, int) {
+        return second, first
+    }
+    func main() {
+        val1, val2 := 10, 20
+        fmt.Println("Before calling function", val1, val2)
+        val1, val2 = swapValues(val1, val2)
+        fmt.Println("After calling function", val1, val2)
+    }
 
-
+    
 
 
 
