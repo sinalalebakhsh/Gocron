d2e65ad69741cafdddb3771af6b6b9f2d092b361
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-04 16:57:34 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -957,9 +957,31 @@ Output:
 
 
 80.Functions as Arguments
-    
+    example:
+        func calcWithTax(price float64) float64 {
+            return price + (price * 0.2)
+        }
+        func calcWithoutTax(price float64) float64 {
+            return price
+        }
+        func printPrice(product string, price float64, calculator func(float64) float64 ) {
+            fmt.Println("Product:", product, "Price:", calculator(price))
+        }
+        func main() {
+            products := map[string]float64 {
+                "Kayak" : 275,
+                "Lifejacket": 48.95,
+            }
+            for product, price := range products {
+                if (price > 100) {
+                    printPrice(product, price, calcWithTax)
+                } else {
+                    printPrice(product, price, calcWithoutTax)
+                }
+            }
+        }
 
-
+81.
 
 
 
