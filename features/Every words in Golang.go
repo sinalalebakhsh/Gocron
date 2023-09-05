a3c22f0597da1e5e38e58d21125c97b2f86d4a3b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 04:57:39 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1302,7 +1302,46 @@ Output:
             writeName(item)
         }
 
-
+97.Creating Arrays, Slices, and Maps Containing Struct Values
+    Omitting the Struct Type:
+        package main
+        import "fmt"
+        func main() {
+            type Product struct {
+                name, category string
+                price float64
+                //otherNames []string
+            }
+            type StockLevel struct {
+                Product
+                Alternate Product
+                count int
+            }
+            array := [1]StockLevel {
+                {
+                    Product: Product { "Kayak", "Watersports", 275.00 },
+                    Alternate: Product{"Lifejacket", "Watersports", 48.95 },
+                    count: 100,
+                },
+            }
+            fmt.Println("Array:", array[0].Product.name)
+            slice := []StockLevel {
+                {
+                    Product: Product { "Kayak", "Watersports", 275.00 },
+                    Alternate: Product{"Lifejacket", "Watersports", 48.95 },
+                    count: 100,
+                },
+            }
+            fmt.Println("Slice:", slice[0].Product.name)
+            kvp := map[string]StockLevel {
+                "kayak": {
+                    Product: Product { "Kayak", "Watersports", 275.00 },
+                    Alternate: Product{"Lifejacket", "Watersports", 48.95 },
+                    count: 100,
+                },
+            }
+            fmt.Println("Map:", kvp["kayak"].Product.name)
+        }
 
 
 
