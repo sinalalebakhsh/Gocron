996695c1414f438cfcdc02078e9a1397a8e17ae8
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 05:17:56 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1525,9 +1525,35 @@ Output:
     Methods are defined using the func keyword, but with the addition of a receiver.
     Interfaces are defined using the type and interface keywords.
 
-    
+    Are there any pitfalls or limitations?
+    Careful use of pointers is important when creating methods, and care must be taken
+    when using interfaces to avoid problems with the underlying dynamic types.
 
-
+107.Defining and Using Method
+    example:
+        package main
+        import "fmt"
+        type Product struct {
+            name, category string
+            price float64
+        }
+        func newProduct(name, category string, price float64) *Product {
+                return &Product{ name, category, price }
+            }
+            func (product *Product) printDetails() {
+                fmt.Println("Name:", product.name, "Category:", product.category,
+                    "Price", product.price)
+            }
+        func main() {
+            products := []*Product {
+                newProduct("Kayak", "Watersports", 275),
+                newProduct("Lifejacket", "Watersports", 48.95),
+                newProduct("Soccer Ball", "Soccer", 19.50),
+            }
+            for _, p := range products {
+                p.printDetails()
+            }
+        }
 
 
 
