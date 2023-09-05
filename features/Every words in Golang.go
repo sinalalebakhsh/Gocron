95be0638f64b24f85c7f4ec4a36d760cb2fc7ab1
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 05:29:48 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1600,7 +1600,30 @@ Output:
             }
         }
 
-
+110.an Interface in a Function
+    example:
+        package main
+        import "fmt"
+        type Expense interface {
+            getName() string
+            getCost(annual bool) float64
+        }
+        func calcTotal(expenses []Expense) (total float64) {
+            for _, item := range expenses {
+                total += item.getCost(true)
+            }
+            return
+        }
+        func main() {
+            expenses := []Expense {
+                Product { "Kayak", "Watersports", 275 },
+                Service {"Boat Cover", 12, 89.50 },
+            }
+            for _, expense := range expenses {
+                fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
+            }
+            fmt.Println("Total:", calcTotal(expenses))
+        }
 
 
 
