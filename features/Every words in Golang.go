94879c59f10095ea4555b91daddaa6dc21e091c7
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 05:30:53 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1625,6 +1625,37 @@ Output:
             fmt.Println("Total:", calcTotal(expenses))
         }
 
+111.an Interface for Struct Fields
+    example:
+        package main
+        import "fmt"
+        type Expense interface {
+            getName() string
+            getCost(annual bool) float64
+        }
+        func calcTotal(expenses []Expense) (total float64) {
+            for _, item := range expenses {
+                total += item.getCost(true)
+            }
+            return
+        }
+        type Account struct {
+            accountNumber int
+            expenses []Expense
+        }
+        func main() {
+            account := Account {
+                accountNumber: 12345,
+                expenses: []Expense {
+                    Product { "Kayak", "Watersports", 275 },
+                    Service {"Boat Cover", 12, 89.50 },
+                 },
+            }
+            for _, expense := range account.expenses {
+                fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
+            }
+            fmt.Println("Total:", calcTotal(account.expenses))
+        }
 
 
 
