c8fe62cda7271e3a7335a53bc52a7760c85a384b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 05:10:56 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1482,6 +1482,127 @@ Output:
                     p.Supplier.name, p.Supplier.city)
             }
         }
+105.Pointer Field Copying
+    Care must be taken when copying structs to consider the effect on pointer fields
+    
+    example:
+        package main
+        import "fmt"
+        type Product struct {
+            name, category string
+            price float64
+            *Supplier
+        }
+        type Supplier struct {
+            name, city string
+        }
+        func newProduct(name, category string, price float64, supplier *Supplier) *Product {
+            return &Product{name, category, price -10, supplier}
+        }
+        func main() {
+            acme := &Supplier { "Acme Co", "New York"}
+            p1 := newProduct("Kayak", "Watersports", 275, acme)
+            p2 := *p1
+            p1.name = "Original Kayak"
+            p1.Supplier.name = "BoatCo"
+            for _, p := range []Product { *p1, p2 } {
+                fmt.Println("Name:", p.name, "Supplier:",
+                    p.Supplier.name, p.Supplier.city)
+            }
+        }
+
+106.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
