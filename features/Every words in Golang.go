a2d4e1c6dbd278848317037d442d46edf8071e6b
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 04:49:22 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1261,7 +1261,19 @@ Output:
             fmt.Println("Alt Name:", stockItem.Alternate.name)
         }
 
-
+95.Comparing Struct Values
+    example:
+        func main() {
+            type Product struct {
+                name, category string
+                price float64
+            }
+            p1 := Product { name: "Kayak", category: "Watersports", price: 275.00 }
+            p2 := Product { name: "Kayak", category: "Watersports", price: 275.00 }
+            p3 := Product { name: "Kayak", category: "Boats", price: 275.00 }
+            fmt.Println("p1 == p2:", p1 == p2)
+            fmt.Println("p1 == p3:", p1 == p3)
+        }
 
 
 
