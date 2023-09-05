d2c7a2b01a75a7ab9202654824818f544552efa6
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 05:05:35 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1418,8 +1418,28 @@ Output:
 
 
 
-
-
+102.Struct Constructor Functions
+    A constructor function is responsible for creating struct values using values received through parameters
+    
+    example:
+        package main
+        import "fmt"
+        type Product struct {
+            name, category string
+            price float64
+        }
+        func newProduct(name, category string, price float64) *Product {
+            return &Product{name, category, price}
+        }
+        func main() {
+            products := [2]*Product {
+                newProduct("Kayak", "Watersports", 275),
+                newProduct("Hat", "Skiing", 42.50),
+            }
+            for _, p := range products {
+                fmt.Println("Name:", p.name, "Category:",  p.category, "Price", p.price)
+            }
+    }
 
 
 
