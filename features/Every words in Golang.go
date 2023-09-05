b8a368f77bcc1886a94403fb4809985888a626e7
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 05:28:24 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1582,7 +1582,23 @@ Output:
         }
 
 
-
+109.Defining and Using Interfaces
+    example:
+        package main
+        import "fmt"
+        type Expense interface {
+            getName() string
+            getCost(annual bool) float64
+        }
+        func main() {
+            expenses := []Expense {
+                Product { "Kayak", "Watersports", 275 },
+                Service {"Boat Cover", 12, 89.50 },
+            }
+            for _, expense := range expenses {
+                fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
+            }
+        }
 
 
 
