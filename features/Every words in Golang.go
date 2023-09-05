c7a67257cdfddfb0027d4637755da6793e1ac513
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 04:25:47 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1100,8 +1100,43 @@ Output:
     }
 
 
+87.Function Closure
+    Functions defined using the literal syntax can reference variables from the surrounding code, a feature
+    known as closure.
 
-
+    example:
+        type calcFunc func(float64) float64
+        func printPrice(product string, price float64, calculator calcFunc) {
+            fmt.Println("Product:", product, "Price:", calculator(price))
+        }
+        func main() {
+            watersportsProducts := map[string]float64 {
+                "Kayak" : 275,
+                "Lifejacket": 48.95,
+            }
+            soccerProducts := map[string] float64 {
+                "Soccer Ball": 19.50,
+                "Stadium": 79500,
+            }
+            calc := func(price float64) float64 {
+                if (price > 100) {
+                    return price + (price * 0.2)
+                }
+                return price;
+            }
+            for product, price := range watersportsProducts {
+                printPrice(product, price, calc)
+            }
+            calc = func(price float64) float64 {
+                if (price > 50) {
+                    return price + (price * 0.1)
+                }
+                return price
+            }
+            for product, price := range soccerProducts {
+                printPrice(product, price, calc)
+            }
+        }
 
 
 
