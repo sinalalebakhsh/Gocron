c12801cecc703bceaf3119c24940e423a50f8c6e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-05 07:30:28 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1687,7 +1687,57 @@ Output:
     any type, which can be a useful way to group disparate types that share no common features
     The empty interface represents all types, including the built-in types and any structs and interfaces
     that have been defined.
+
+114.Empty Interface for Function Parameters
+    The empty interface can be used as the type for a function parameter, allowing a function to be called with
+    any value
     
+    example:
+        package main
+        import "fmt"
+        type Expense interface {
+            getName() string
+            getCost(annual bool) float64
+        }
+        type Person struct {
+            name, city string
+        }
+        func processItem(item interface{}) {
+                switch value := item.(type) {
+                    case Product:
+                        fmt.Println("Product:", value.name, "Price:", value.price)
+                    case *Product:
+                        fmt.Println("Product Pointer:", value.name, "Price:", value.price)
+                    case Service:
+                        fmt.Println("Service:", value.description, "Price:",
+                            value.monthlyFee * float64(value.durationMonths))
+                    case Person:
+                        fmt.Println("Person:", value.name, "City:", value.city)
+                    case *Person:
+                        fmt.Println("Person Pointer:", value.name, "City:", value.city)
+                    case string, bool, int:
+                        fmt.Println("Built-in type:", value)
+                    default:
+                        fmt.Println("Default:", value)
+                }
+            }
+        func main() {
+            var expense Expense = &Product { "Kayak", "Watersports", 275 }
+            data := []interface{} {
+                expense,
+                Product { "Lifejacket", "Watersports", 48.95 },
+                Service {"Boat Cover", 12, 89.50, []string{} },
+                Person { "Alice", "London"},
+                &Person { "Bob", "New York"},
+                "This is a string",
+                100,
+                true,
+            }
+            for _, item := range data {
+                processItem(item)
+            }
+        }
+
 
 
 
