110fed13326cd74d1f9472c2ce556ee9c4b8c4e0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-06 05:30:50 -0400

    ++

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -14,25 +15,39 @@ import (
 func main()  {
 
 
-	AllArgs := os.Args 
+	HelpFlag := flag.String("-h", "help", "--help")
+	HelpFlagPlus := flag.String("-help", "-HELP", "--HELP")
+	HelpFlagPlusPlus := flag.String("HELP", "--h", "helpme!")
 
+	var SliceOfFlags []string 
+	SliceOfFlags = append(SliceOfFlags, *HelpFlag)
+	SliceOfFlags = append(SliceOfFlags, *HelpFlagPlus)
+	SliceOfFlags = append(SliceOfFlags, *HelpFlagPlusPlus)
 
+	for _, Value := range SliceOfFlags {
+		fmt.Println(Value)
+	}
 
-	if AllArgs[1] == strings.ToLower("-h") || AllArgs[1] == strings.ToLower("help") {
-		fmt.Println(`
+	HelpMessage := `
 Gocron Project:
+-------------------------------------------------------
 is a API for learning GO language with example.
+-------------------------------------------------------
 my name is Sina LalehBakhsh, I hope this API is useful for you
 after running program, write your single word about any of GO language.
 if your perpuse is more than one word, for convenience searching, just write keywords.
 like this:
-	map slice `)
-	} else if AllArgs[1] == strings.ToLower("all") || 
-	AllArgs[1] == strings.ToLower("everyexample") || 
-	AllArgs[1] == strings.ToLower("examples") {
-		color.Cyan(fmt.Sprintln(features.OriginalFeatures))
-	} 
+	map slice
+-------------------------------------------------------`
 
+	// if AllArgs[1] == strings.ToLower("-h") || AllArgs[1] == strings.ToLower("help") {
+	// 	fmt.Println()
+	// } else if AllArgs[1] == strings.ToLower("all") || 
+	// AllArgs[1] == strings.ToLower("everyexample") || 
+	// AllArgs[1] == strings.ToLower("examples") {
+	// 	} 
+		
+	color.Cyan(fmt.Sprintln(features.OriginalFeatures))
 
 
 
