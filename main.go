4e58fcf5a60bd1847839a97857c377d09f38604c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-06 04:21:00 -0400

    ++

--- main.go
+++ main.go
@@ -19,9 +19,13 @@ func main()  {
 Gocron Project:
 is a API for learning GO language with example.
 my name is Sina LalehBakhsh, I hope this API is useful for you`)
+	} else if os.Args[1] == strings.ToLower("all") || 
+	os.Args[1] == strings.ToLower("everyexample") || 
+	os.Args[1] == strings.ToLower("examples") {
+		color.Cyan(fmt.Sprintln(features.OriginalFeatures))
 	}
 
-	color.Cyan(fmt.Sprintln(features.OriginalFeatures))
+
 
 
 
