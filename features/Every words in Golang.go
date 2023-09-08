029409dd5c5910f60b5ce1b5adf98a6ab697cd75
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 07:29:33 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1461,30 +1461,33 @@ Output:
 
 104.Pointer Types for Struct Fields
     example:
-        package main
-        import "fmt"
-        type Product struct {
-            name, category string
-            price float64
-            *Supplier
-        }
-        type Supplier struct {
-            name, city string
-        }
-        func newProduct(name, category string, price float64, supplier *Supplier) *Product {
-            return &Product{name, category, price -10, supplier}
-        }
-        func main() {
-            acme := &Supplier { "Acme Co", "New York"}
-            products := [2]*Product {
-                newProduct("Kayak", "Watersports", 275, acme),
-                newProduct("Hat", "Skiing", 42.50, acme),
-            }
-            for _, p := range products {
-                fmt.Println("Name:", p.name, "Supplier:",
-                    p.Supplier.name, p.Supplier.city)
-            }
-        }
+    |    package main
+    |    import "fmt"
+    |    type Product struct {
+    |        name, category string
+    |        price float64
+    |        *Supplier
+    |    }
+    |    type Supplier struct {
+    |        name, city string
+    |    }
+    |    func newProduct(name, category string, price float64, supplier *Supplier) *Product {
+    |        return &Product{name, category, price -10, supplier}
+    |    }
+    |    func main() {
+    |        acme := &Supplier { "Acme Co", "New York"}
+    |        products := [2]*Product {
+    |            newProduct("Kayak", "Watersports", 275, acme),
+    |            newProduct("Hat", "Skiing", 42.50, acme),
+    |        }
+    |        for _, p := range products {
+    |            fmt.Println("Name:", p.name, "Supplier:",
+    |                p.Supplier.name, p.Supplier.city)
+    |        }
+    |    }
+    |________________________________________________________________________________________
+
+    
 105.Pointer Field Copying
     Care must be taken when copying structs to consider the effect on pointer fields
     
@@ -1515,7 +1518,7 @@ Output:
     |    }
     |______________________________________________________________________________________________
 
-    
+
 106.Method
     What are they?
     Methods are functions that are invoked on a struct and have access to all of the
