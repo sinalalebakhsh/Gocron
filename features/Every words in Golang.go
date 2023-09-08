072a2af49a65df5f4a981797dc9aa218b2a33e98
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 07:09:15 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -2341,7 +2341,31 @@ Output:
     |    Key: Ball Price: 23.4
     |____________________________________
 
-
+135.the Type Switch Limitation
+    
+    example:
+    main.go:
+    |    package main
+    |    import (
+    |        "fmt"
+    |        "composition/store"
+    |    )
+    |    func main() {
+    |        products := map[string]store.ItemForSale {
+    |            "Kayak": store.NewBoat("Kayak", 279, 1, false),
+    |            "Ball": store.NewProduct("Soccer Ball", "Soccer", 19.50),
+    |        }
+    |        for key, p := range products {
+    |            switch item := p.(type) {
+    |                case *store.Product, *store.Boat:
+    |                    fmt.Println("Name:", item.Name, "Category:", item.Category,
+    |                        "Price:", item.Price(0.2))
+    |                default:
+    |                    fmt.Println("Key:", key, "Price:", p.Price(0.2))
+    |            }
+    |        }
+    |    }
+    |________________________________________________________________
 
 
 
