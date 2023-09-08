389886cdbfdb8e5a2ec0da1f25eab0f4481f5817
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 07:28:14 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1560,30 +1560,30 @@ Output:
 
 108.Defining Method Parameters and Results
     example:
-        package main
-        import "fmt"
-        type Product struct {
-            name, category string
-            price float64
-        }
-        func newProduct(name, category string, price float64) *Product {
-                return &Product{ name, category, price }
-            }
-            func (product *Product) printDetails() {
-                fmt.Println("Name:", product.name, "Category:", product.category,
-                    "Price", product.price)
-            }
-        func main() {
-            products := []*Product {
-                newProduct("Kayak", "Watersports", 275),
-                newProduct("Lifejacket", "Watersports", 48.95),
-                newProduct("Soccer Ball", "Soccer", 19.50),
-            }
-            for _, p := range products {
-                p.calcTax(0.2, 100) //<-------------------------------here
-            }
-        }
-
+    |    package main
+    |    import "fmt"
+    |    type Product struct {
+    |        name, category string
+    |        price float64
+    |    }
+    |    func newProduct(name, category string, price float64) *Product {
+    |            return &Product{ name, category, price }
+    |        }
+    |        func (product *Product) printDetails() {
+    |            fmt.Println("Name:", product.name, "Category:", product.category,
+    |                "Price", product.price)
+    |        }
+    |    func main() {
+    |        products := []*Product {
+    |            newProduct("Kayak", "Watersports", 275),
+    |            newProduct("Lifejacket", "Watersports", 48.95),
+    |            newProduct("Soccer Ball", "Soccer", 19.50),
+    |        }
+    |        for _, p := range products {
+    |            p.calcTax(0.2, 100) //<-------------------------------here
+    |        }
+    |    }
+    |_____________________________________________________________________________________
 
 109.Defining and Using Interfaces
     One interface can enclose another, with the effect that types must implement all the methods defined
