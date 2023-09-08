1091ede29fbf928e6e04597148c63251900b0624
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 07:11:10 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -2373,8 +2373,31 @@ Output:
     |    Category)
     |_____________________________________________________________________________
 
-    
-
+    Using Separate case Statements in the main.go:
+    |    package main
+    |    import (
+    |       "fmt"
+    |       "composition/store"
+    |    )
+    |    func main() {
+    |       products := map[string]store.ItemForSale {
+    |          "Kayak": store.NewBoat("Kayak", 279, 1, false),
+    |          "Ball": store.NewProduct("Soccer Ball", "Soccer", 19.50),
+    |       }
+    |       for key, p := range products {
+    |          switch item := p.(type) {
+    |             case *store.Product:
+    |                fmt.Println("Name:", item.Name, "Category:", item.Category,
+    |                   "Price:", item.Price(0.2))
+    |             case *store.Boat:
+    |                fmt.Println("Name:", item.Name, "Category:", item.Category,
+    |                   "Price:", item.Price(0.2))
+    |             default:
+    |                fmt.Println("Key:", key, "Price:", p.Price(0.2))
+    |          }
+    |       }
+    |    }
+    |__________________________________________________________________________________
 
 
 
