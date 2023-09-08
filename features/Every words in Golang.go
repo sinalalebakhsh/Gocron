b007ca49fd7a3c7e9eeed3e81a841cdcd2fa3933
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 11:42:11 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -699,26 +699,27 @@ Understanding Flow Control
         }
 
     Enumerating a Map in Order:
-        import (
-                "fmt"
-                "sort"
-            )
-        func main() {
-            products := map[string]float64 {
-                "Kayak" : 279,
-                "Lifejacket": 48.95,
-                "Hat": 0,
-            }
-            keys := make([]string, 0, len(products))
-            for key, _ := range products {
-                keys = append(keys, key)
-            }
-            sort.Strings(keys)
-            for _, key := range keys {
-                fmt.Println("Key:", key, "Value:", products[key])
-            }
-        }
-
+    example:
+    |    import (
+    |            "fmt"
+    |            "sort"
+    |        )
+    |    func main() {
+    |        products := map[string]float64 {
+    |            "Kayak" : 279,
+    |            "Lifejacket": 48.95,
+    |            "Hat": 0,
+    |        }
+    |        keys := make([]string, 0, len(products))
+    |        for key, _ := range products {
+    |            keys = append(keys, key)
+    |        }
+    |        sort.Strings(keys)
+    |        for _, key := range keys {
+    |            fmt.Println("Key:", key, "Value:", products[key])
+    |        }
+    |    }
+    |______________________________________________________
         
 
 Understanding the Dual Nature of Strings
