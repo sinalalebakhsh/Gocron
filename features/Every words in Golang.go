6b160300b09a8c8e69c81413fa7aab73e81f9877
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 07:37:24 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -893,26 +893,27 @@ Output:
 
 76.Using Named Results
     example:
-        func calcTax(price float64) (float64, bool) {
-            if (price > 100) {
-                return price * 0.2, true
-            }
-            return 0, false
-        }
-        func calcTotalPrice(products map[string]float64,
-                minSpend float64) (total, tax float64)  {
-            total = minSpend
-            for _, price := range products {
-                if taxAmount, due := calcTax(price); due {
-                    total += taxAmount;
-                    tax += taxAmount
-                } else {
-                    total += price
-                }
-            }
-            return
-        }
-
+    |    func calcTax(price float64) (float64, bool) {
+    |        if (price > 100) {
+    |            return price * 0.2, true
+    |        }
+    |        return 0, false
+    |    }
+    |    func calcTotalPrice(products map[string]float64,
+    |            minSpend float64) (total, tax float64)  {
+    |        total = minSpend
+    |        for _, price := range products {
+    |            if taxAmount, due := calcTax(price); due {
+    |                total += taxAmount;
+    |                tax += taxAmount
+    |            } else {
+    |                total += price
+    |            }
+    |        }
+    |        return
+    |    }
+    |_____________________________________________________
+    
 77.defer
     The defer keyword is used to schedule a function call that will be performed immediately before the current
     function returns
