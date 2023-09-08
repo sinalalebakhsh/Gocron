28ac11d51b88879fda4a0dcbf00ad58467c65d9a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 14:17:55 -0400

    ++

--- features/SingleDefinitions.go
+++ features/SingleDefinitions.go
@@ -19,23 +19,23 @@ var OriginSingleDef SingleDefinitions = SingleDefinitions{
 The go build command compiles the source code in the current directory 
 and generates an executable file.`,
 	
-	"flag": `flag package:
+	"flag": `5.flag package:
 Command-line flags are a common way to specify options for command-line programs. 
 For example, in wc -l the -l is a command-line flag.
 Go provides a flag package supporting basic command-line flag parsing.
 We'll use this package to implement our example command-line program.`,
 	
-	"flag package": `flag package:
+	"flag package": `5.flag package:
 Command-line flags are a common way to specify options for command-line programs. 
 For example, in wc -l the -l is a command-line flag.
 Go provides a flag package supporting basic command-line flag parsing.
 We'll use this package to implement our example command-line program.`,
     
-    "clean":`clean
+    "clean":`2.clean
 The go clean command removes the output produced by the go build command, 
 including the executable and any temporary files that were created during the build.`,
 
-    "Goroutines":`Goroutines and Channels
+    "Goroutines":`138.Goroutines and Channels
 What are they?
 Goroutines are lightweight threads created and managed by the Go runtime.
 Channels are pipes that carry values of a specific type.
@@ -75,7 +75,7 @@ Enumerate the values received           Use a for loop with the range keyword
 from a channel
 `,
 
-"Channels":`Goroutines and Channels
+"Channels":`138.Goroutines and Channels
 What are they?
 Goroutines are lightweight threads created and managed by the Go runtime.
 Channels are pipes that carry values of a specific type.
@@ -116,7 +116,7 @@ from a channel
 `,
 
 
-"Goroutines and Channels":`Goroutines and Channels
+"Goroutines and Channels":`138.Goroutines and Channels
 What are they?
 Goroutines are lightweight threads created and managed by the Go runtime.
 Channels are pipes that carry values of a specific type.
