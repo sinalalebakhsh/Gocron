4d394e9eb79df978783e350d4661fe9929eb889a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 05:59:16 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -2143,10 +2143,24 @@ Output:
     |    Rental Boat: Super Yacht Rental Price: 120000
     |_______________________________________________________________
 
-    
-
-
-
+130.Multiple Nested Types in the Same Struct
+    example:
+    rentalboats.go:
+    |    package store
+    |    type Crew struct {
+    |        Captain, FirstOfficer string
+    |    }
+    |    type RentalBoat struct {
+    |        *Boat
+    |        IncludeCrew bool
+    |        *Crew
+    |    }
+    |    func NewRentalBoat(name string, price float64, capacity int,
+    |            motorized, crewed bool, captain, firstOfficer string) *RentalBoat {
+    |        return &RentalBoat{NewBoat(name, price, capacity, motorized), crewed,
+    |            &Crew{captain, firstOfficer}}
+    |    }
+    |________________________________________________________________
 
 
 
