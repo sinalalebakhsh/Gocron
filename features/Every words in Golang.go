57bce45646dfb5026908e0d461ac098b8b127ff4
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 07:21:27 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -2433,27 +2433,30 @@ Output:
     Using Interfaces in the main.go:
     |    package main
     |    import (
-    |        "fmt"
-    |        "composition/store"
+    |       "fmt"
+    |       "composition/store"
     |    )
     |    func main() {
-    |        products := map[string]store.ItemForSale {
-    |            "Kayak": store.NewBoat("Kayak", 279, 1, false),
-    |            "Ball": store.NewProduct("Soccer Ball", "Soccer", 19.50),
-    |        }
-    |        for key, p := range products {
-    |            switch item := p.(type) {
-    |                case store.Describable:
-    |                    fmt.Println("Name:", item.GetName(), "Category:", item.GetCategory(),
-    |                        "Price:", item.(store.ItemForSale).Price(0.2))
-    |                default:
-    |                    fmt.Println("Key:", key, "Price:", p.Price(0.2))
-    |            }
-    |        }
+    |       products := map[string]store.ItemForSale {
+    |          "Kayak": store.NewBoat("Kayak", 279, 1, false),
+    |          "Ball": store.NewProduct("Soccer Ball", "Soccer", 19.50),
+    |       }
+    |       for key, p := range products {
+    |          switch item := p.(type) {
+    |             case store.Describable:
+    |                fmt.Println("Name:", item.GetName(), "Category:", item.GetCategory(),
+    |                   "Price:", item.(store.ItemForSale).Price(0.2))
+    |             default:
+    |                fmt.Println("Key:", key, "Price:", p.Price(0.2))
+    |          }
+    |       }
     |    }
+    |____________________________________________________________________
+
+    Output:
+    |    Name: Kayak Category: Watersports Price: 334.8
+    |    Name: Soccer Ball Category: Soccer Price: 23.4
     |
-
-
 
 
 
