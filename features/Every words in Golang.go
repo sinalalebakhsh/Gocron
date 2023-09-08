651805cdb35db77d2a2112b93253718fa5273d9d
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 07:20:57 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -2430,6 +2430,28 @@ Output:
     |    }
     |__________________________________________________________________
 
+    Using Interfaces in the main.go:
+    |    package main
+    |    import (
+    |        "fmt"
+    |        "composition/store"
+    |    )
+    |    func main() {
+    |        products := map[string]store.ItemForSale {
+    |            "Kayak": store.NewBoat("Kayak", 279, 1, false),
+    |            "Ball": store.NewProduct("Soccer Ball", "Soccer", 19.50),
+    |        }
+    |        for key, p := range products {
+    |            switch item := p.(type) {
+    |                case store.Describable:
+    |                    fmt.Println("Name:", item.GetName(), "Category:", item.GetCategory(),
+    |                        "Price:", item.(store.ItemForSale).Price(0.2))
+    |                default:
+    |                    fmt.Println("Key:", key, "Price:", p.Price(0.2))
+    |            }
+    |        }
+    |    }
+    |
 
 
 
