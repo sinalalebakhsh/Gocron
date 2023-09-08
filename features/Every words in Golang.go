e842bf4c136581a693ffaa923271a951b4d76b19
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 07:28:44 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1534,30 +1534,32 @@ Output:
 
 107.Defining and Using Method
     example:
-        package main
-        import "fmt"
-        type Product struct {
-            name, category string
-            price float64
-        }
-        func newProduct(name, category string, price float64) *Product {
-                return &Product{ name, category, price }
-            }
-            func (product *Product) printDetails() {
-                fmt.Println("Name:", product.name, "Category:", product.category,
-                    "Price", product.price)
-            }
-        func main() {
-            products := []*Product {
-                newProduct("Kayak", "Watersports", 275),
-                newProduct("Lifejacket", "Watersports", 48.95),
-                newProduct("Soccer Ball", "Soccer", 19.50),
-            }
-            for _, p := range products {
-                p.printDetails()
-            }
-        }
+    |    package main
+    |    import "fmt"
+    |    type Product struct {
+    |        name, category string
+    |        price float64
+    |    }
+    |    func newProduct(name, category string, price float64) *Product {
+    |            return &Product{ name, category, price }
+    |        }
+    |        func (product *Product) printDetails() {
+    |            fmt.Println("Name:", product.name, "Category:", product.category,
+    |                "Price", product.price)
+    |        }
+    |    func main() {
+    |        products := []*Product {
+    |            newProduct("Kayak", "Watersports", 275),
+    |            newProduct("Lifejacket", "Watersports", 48.95),
+    |            newProduct("Soccer Ball", "Soccer", 19.50),
+    |        }
+    |        for _, p := range products {
+    |            p.printDetails()
+    |        }
+    |    }
+    |____________________________________________________________________________
 
+    
 108.Defining Method Parameters and Results
     example:
     |    package main
