f5e2d4b57673dd0f326b466fb8ea50d87c3567fa
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 07:34:12 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -1106,39 +1106,39 @@ Output:
     known as closure.
 
     example:
-        type calcFunc func(float64) float64
-        func printPrice(product string, price float64, calculator calcFunc) {
-            fmt.Println("Product:", product, "Price:", calculator(price))
-        }
-        func main() {
-            watersportsProducts := map[string]float64 {
-                "Kayak" : 275,
-                "Lifejacket": 48.95,
-            }
-            soccerProducts := map[string] float64 {
-                "Soccer Ball": 19.50,
-                "Stadium": 79500,
-            }
-            calc := func(price float64) float64 {
-                if (price > 100) {
-                    return price + (price * 0.2)
-                }
-                return price;
-            }
-            for product, price := range watersportsProducts {
-                printPrice(product, price, calc)
-            }
-            calc = func(price float64) float64 {
-                if (price > 50) {
-                    return price + (price * 0.1)
-                }
-                return price
-            }
-            for product, price := range soccerProducts {
-                printPrice(product, price, calc)
-            }
-        }
-
+    |    type calcFunc func(float64) float64
+    |    func printPrice(product string, price float64, calculator calcFunc) {
+    |        fmt.Println("Product:", product, "Price:", calculator(price))
+    |    }
+    |    func main() {
+    |        watersportsProducts := map[string]float64 {
+    |            "Kayak" : 275,
+    |            "Lifejacket": 48.95,
+    |        }
+    |        soccerProducts := map[string] float64 {
+    |            "Soccer Ball": 19.50,
+    |            "Stadium": 79500,
+    |        }
+    |        calc := func(price float64) float64 {
+    |            if (price > 100) {
+    |                return price + (price * 0.2)
+    |            }
+    |            return price;
+    |        }
+    |        for product, price := range watersportsProducts {
+    |            printPrice(product, price, calc)
+    |        }
+    |        calc = func(price float64) float64 {
+    |            if (price > 50) {
+    |                return price + (price * 0.1)
+    |            }
+    |            return price
+    |        }
+    |        for product, price := range soccerProducts {
+    |            printPrice(product, price, calc)
+    |        }
+    |    }
+    |_____________________________________________________________
 
         
 88.struct
@@ -1170,7 +1170,7 @@ Output:
     |        fmt.Println("Changed price:", kayak.price)
     |    }
     |_________________________________________________________________
-    
+
     Go doesn't differentiate between structs and classes, in the way that other languages do. All custom
     data types are defined as structs, and the decision to pass them by reference or by value is made
     depending on whether a pointer is used.
