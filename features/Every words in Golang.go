4b16fa488ac906ced76284a8cf09707a216cced9
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-08 06:00:31 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -2162,6 +2162,25 @@ Output:
     |    }
     |________________________________________________________________
 
+    Using Promoted Fields in the main.go:
+    |    package main
+    |    import (
+    |       "fmt"
+    |       "composition/store"
+    |    )
+    |    func main() {
+    |       rentals := []*store.RentalBoat {
+    |          store.NewRentalBoat("Rubber Ring", 10, 1, false, false, "N/A", "N/A"),
+    |          store.NewRentalBoat("Yacht", 50000, 5, true, true, "Bob", "Alice"),
+    |          store.NewRentalBoat("Super Yacht", 100000, 15, true, true,
+    |             "Dora", "Charlie"),
+    |       }
+    |       for _, r := range rentals {
+    |          fmt.Println("Rental Boat:", r.Name, "Rental Price:", r.Price(0.2),
+    |             "Captain:", r.Captain)
+    |       }
+    |    }
+    |___________________________________________________________________
 
 
 
