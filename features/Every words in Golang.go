36ebbb1838edcf21899874f30fc2e79bc69a369d
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-09 13:24:10 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -3307,6 +3307,31 @@ Converting a String to Runes
         Upper: Ǉ [199 135]
         Title: ǈ [199 136]
 
+165.Working with Character Case
+    The unicode package provides functions that can be used to determine or change the case of individual
+    characters
+    Functions in the unicode Package for Character Case
+    Function        Description
+    -------------   -----------------------------------------------------------------
+    IsLower(rune)   This function returns true if the specified rune is lowercase.
+    ToLower(rune)   This function returns the lowercase rune associated with the specified rune.
+    IsUpper(rune)   This function returns true if the specified rune is uppercase.
+    ToUpper(rune)   This function returns the upper rune associated with the specified rune.
+    IsTitle(rune)   This function returns true if the specified rune is title case.
+    ToTitle(rune)   This function returns the title case rune associated with the specified rune.
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
