1226ca5e04e7f0f0e267cb21fbaef28375055f7e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-09 17:20:43 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -3633,8 +3633,25 @@ Converting a String to Runes
     Ourput:
         Trimmed:>>boat for one pers<<
 
+177.TrimPrefix and TrimSuffix functions
+    example:
+        package main
+        import (
+            "fmt"
+            "strings"
+        )
+        func main() {
+            description := "A boat for one person"
+            prefixTrimmed := strings.TrimPrefix(description, "A boat ")
+            wrongPrefix := strings.TrimPrefix(description, "A hat ")
+            fmt.Println("Trimmed:", prefixTrimmed)
+            fmt.Println("Not trimmed:", wrongPrefix)
+        }
+    Output:
+        Trimmed: for one person
+        Not trimmed: A boat for one person
 
-
+        
 
 
 
