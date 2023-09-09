3ede9a51defc72accb77851a01b01f40843c054c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-09 13:32:06 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -3359,8 +3359,30 @@ Converting a String to Runes
     LastIndexFunc(s, func)  character in the string s for which the specified function returns true, as
                             described in the “Inspecting Strings with Custom Functions” section.
 
-
-
+    
+    example:
+        package main
+        import (
+            "fmt"
+            "strings"
+            //"unicode"
+        )
+        func main() {
+            description := "A boat for one person"
+            fmt.Println("Count:", strings.Count(description, "o"))
+            fmt.Println("Index:", strings.Index(description, "o"))
+            fmt.Println("LastIndex:", strings.LastIndex(description, "o"))
+            fmt.Println("IndexAny:", strings.IndexAny(description, "abcd"))
+            fmt.Println("LastIndex:", strings.LastIndex(description, "o"))
+            fmt.Println("LastIndexAny:", strings.LastIndexAny(description, "abcd"))
+        }
+    Output:
+        Count: 4
+        Index: 3
+        LastIndex: 19
+        IndexAny: 2
+        LastIndex: 19
+        LastIndexAny: 4
 
 
 
