0c405092ba606a7fa37277b79c4dedc591ffcc18
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-09 14:41:38 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -3412,6 +3412,53 @@ Converting a String to Runes
         IndexFunc: 2
 
 
+169.Splitting Strings
+    The Functions for Splitting Strings in the strings Package
+    Function                Description
+    ---------------         --------------------------------
+    Fields(s)               This function splits a string on whitespace characters and returns a slice
+                            containing the nonwhitespace sections of the string s.
+
+    FieldsFunc(s, func)     This function splits the string s on the characters for which a custom function
+                            returns true and returns a slice containing the remaining sections of the string.
+
+    Split(s, sub)           This function splits the string s on every occurrence of the specified substring,
+                            returning a string slice. If the separator is the empty string, then the slice will
+                            contain strings for each character.
+
+    SplitN(s, sub, max)     This function is similar to Split, but accepts an additional int argument that
+                            specifies the maximum number of substrings to return. The last substring in the
+                            result slice will contain the unsplit portion of the source string.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
