3784f886ad5ce29bb484386e5081fd18b5a56de4
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-10 08:19:24 -0400

    ++

--- Server/Server.go
+++ Server/Server.go
@@ -3,17 +3,22 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 
 	"github.com/sinalalebakhsh/Gocron/features"
 	"github.com/gorilla/mux"
-
-	"html/template"
+	"github.com/go-redis/redis"
 )
 
+var client *redis.Client
 var templates *template.Template
 
 func HandlerAllFeatures(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "index.html", nil)
+	comments, err := client.LRange("comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
 }
 
 func HandlerSingleDefinitions(w http.ResponseWriter, r *http.Request) {
@@ -24,6 +29,9 @@ func HandlerSingleDefinitions(w http.ResponseWriter, r *http.Request) {
 
 
 func Servers() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	router := mux.NewRouter()
 	http.Handle("/", router)
