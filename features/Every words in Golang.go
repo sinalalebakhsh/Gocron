ac9267996a5e6584d28490d3f965f90c13666f3e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-10 08:22:38 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -3682,7 +3682,10 @@ Converting a String to Runes
     ReplaceAll(s, old, new)     This function alters the string s by replacing all occurrences of the string old with
                                 the string new. Unlike the Replace function, there is no limit on the number of
                                 occurrences that will be replaced.
-
+                                
+    Map(func, s)                This function generates a string by invoking the custom function for each character in
+                                the string s and concatenating the results. If the function produces a negative value,
+                                the current character is dropped without a replacement.
 
 
 
