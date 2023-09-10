8fa4a1c69c604ac36bdd0898cf2102f5f1b18667
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-10 10:44:52 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -3762,6 +3762,15 @@ Converting a String to Runes
                             and write the results to an io.Writer
 
 
+184.Building and Generating Strings
+    The strings Functions for Generating Strings
+    Function            Description
+    ----------------    ----------------------------------------------------------------------------------------
+    Join(slice, sep)    This function combines the elements in the specified string slice, with the specified
+                        separator string placed between elements.
+
+    Repeat(s, count)    This function generates a string by repeating the string s for a specified number of times.
+
 
 
 
