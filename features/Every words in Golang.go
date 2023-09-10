3a074b550cc8a3b379a3786ef5aa6487a766edb5
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-10 08:41:41 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -3752,6 +3752,16 @@ Converting a String to Runes
         Replaced: It was a kayak. A huge kayak.   222
         
 
+183.The Replacer Methods
+    Name                    Description
+    -------------------     --------------------------------------------
+    Replace(s)              This method returns a string for which all the replacements specified with the
+                            constructor have been performed on the string s.
+
+    WriteString(writer, s)  This method is used to perform the replacements specified with the constructor
+                            and write the results to an io.Writer
+
+
 
 
 
