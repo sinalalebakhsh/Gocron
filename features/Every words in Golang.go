aca4bdc3a34c15a0d071162d71f838f0b2ecd32c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-12 09:23:21 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -4079,7 +4079,35 @@ go mod tidy126.Putting Type and Interface Composition in Context
         Product: 1234 Price: 5555
         Product:1234Price:5555
 ████████████████████████████████████████████████████████████████████████
-205.
+205.fmt.Printf
+        example:
+            package main
+            import "fmt"
+            type Product struct {
+                Name, Category string
+                Price float64
+            }
+            var Kayak = Product {
+                Name: "Kayak",
+                Category: "Watersports",
+                Price: 275,
+            }
+            var Products = []Product {
+                { "Kayak", "Watersports", 279 },
+                { "Lifejacket", "Watersports", 49.95 },
+                { "Soccer Ball", "Soccer", 19.50 },
+                { "Corner Flags", "Soccer", 34.95 },
+                { "Stadium", "Soccer", 79500 },
+                { "Thinking Cap", "Chess", 16 },
+                { "Unsteady Chair", "Chess", 75 },
+                { "Bling-Bling King", "Chess", 1200 },
+            }
+            func main() {
+                fmt.Printf("Product: %v, Price: $%4.2f", Kayak.Name, Kayak.Price)
+            }
+        Output:
+            
+
 ████████████████████████████████████████████████████████████████████████
 206.
 ████████████████████████████████████████████████████████████████████████
