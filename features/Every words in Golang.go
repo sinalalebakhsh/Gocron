df0341c9f0a08e255a14437f26ce29b77946dc68
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-12 14:06:48 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -4198,9 +4198,39 @@ go mod tidy126.Putting Type and Interface Composition in Context
     %T      This verb displays the Go type of a value.
 
     example:
-    
+        package main
+        import "fmt"
+        type Product struct {
+            Name, Category string
+            Price          float64
+        }
+        var Kayak = Product{
+            Name:     "Kayak",
+            Category: "Watersports",
+            Price:    275,
+        }
+        var Products = []Product{
+            {"Kayak", "Watersports", 279},
+            {"Lifejacket", "Watersports", 49.95},
+            {"Soccer Ball", "Soccer", 19.50},
+            {"Corner Flags", "Soccer", 34.95},
+            {"Stadium", "Soccer", 79500},
+            {"Thinking Cap", "Chess", 16},
+            {"Unsteady Chair", "Chess", 75},
+            {"Bling-Bling King", "Chess", 1200},
+        }
+        func Printfln(template string, values ...interface{}) {
+            fmt.Printf(template+"\n", values...)
+        }
+        func main() {
+            Printfln("Value: %v", Kayak)
+            Printfln("Go syntax: %#v", Kayak)
+            Printfln("Type: %T", Kayak)
+        }
     Output:
-
+        Value: {Kayak Watersports 275}
+        Go syntax: main.Product{Name:"Kayak", Category:"Watersports", Price:275}
+        Type: main.Product
 ████████████████████████████████████████████████████████████████████████
 209.
 ████████████████████████████████████████████████████████████████████████
