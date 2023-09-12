e6c8fd6437ceaf3da0db4b8360b520a3805060a9
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-12 14:22:16 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -4269,7 +4269,41 @@ go mod tidy126.Putting Type and Interface Composition in Context
         Value: {Kayak Watersports 275}
         Value with fields: {Name:Kayak Category:Watersports Price:275}
 ████████████████████████████████████████████████████████████████████████
-210.
+210.Custom Struct Format
+    example:
+        package main
+        import "fmt"
+        type Product struct {
+            Name, Category string
+            Price          float64
+        }
+        var Kayak = Product{
+            Name:     "Kayak",
+            Category: "Watersports",
+            Price:    275,
+        }
+        var Products = []Product{
+            {"Kayak", "Watersports", 279},
+            {"Lifejacket", "Watersports", 49.95},
+            {"Soccer Ball", "Soccer", 19.50},
+            {"Corner Flags", "Soccer", 34.95},
+            {"Stadium", "Soccer", 79500},
+            {"Thinking Cap", "Chess", 16},
+            {"Unsteady Chair", "Chess", 75},
+            {"Bling-Bling King", "Chess", 1200},
+        }
+        func Printfln(template string, values ...interface{}) {
+            fmt.Printf(template+"\n", values...)
+        }
+        func (p Product) String() string {
+            return fmt.Sprintf("Product: %v, Price: $%4.2f", p.Name, p.Price)
+        }
+        func main() {
+            Printfln("Value: %v", Kayak)
+            Printfln("Value with fields: %+v", Kayak)
+        }
+    Output:
+    
 ████████████████████████████████████████████████████████████████████████
 211.
 ████████████████████████████████████████████████████████████████████████
