5b953e9d35eb8188bf63ad5f3501b40d352f68e4
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-13 04:39:02 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -4599,7 +4599,28 @@ go mod tidy126.Putting Type and Interface Composition in Context
     Output:
       Error: unexpected newline
 ████████████████████████████████████████████████████████████████████████
-223.
+223.Sscan Function
+    example:
+        package main
+        import "fmt"
+        func Printfln(template string, values ...interface{}) {
+            fmt.Printf(template + "\n", values...)
+        }
+        func main() {
+            var name string
+            var category string
+            var price float64
+            source := "Lifejacket Watersports 48.95"
+            n, err := fmt.Sscan(source, &name, &category, &price)
+            if (err == nil) {
+                Printfln("Scanned %v values", n)
+                Printfln("Name: %v, Category: %v, Price: %.2f", name, category, price)
+            } else {
+                Printfln("Error: %v", err.Error())
+            }
+        }
+    Output:
+        
 ████████████████████████████████████████████████████████████████████████
 224.
 ████████████████████████████████████████████████████████████████████████
