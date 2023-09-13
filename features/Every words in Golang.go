e2a71c1e491270c5d184e7f6c2b65a80cedcec58
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-13 02:45:03 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -4372,7 +4372,20 @@ go mod tidy126.Putting Type and Interface Composition in Context
             uppercase (%X) letters.
 
     example:
+        package main
+        import "fmt"
+        func Printfln(template string, values ...interface{}) {
+            fmt.Printf(template + "\n", values...)
+        }
+        func main() {
+            number := 279.00
+            Printfln("Decimalless with exponent: %b", number)
+            Printfln("Decimal with exponent: %e", number)
+            Printfln("Decimal without exponent: %f", number)
+            Printfln("Hexadecimal: %x, %X", number, number)
+        }
     Output:
+        
 ████████████████████████████████████████████████████████████████████████
 214.
 ████████████████████████████████████████████████████████████████████████
