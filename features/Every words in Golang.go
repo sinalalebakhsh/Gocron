310b77901a651c2506e2b33125f3e069cb955163
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-13 02:40:23 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -4329,6 +4329,25 @@ go mod tidy126.Putting Type and Interface Composition in Context
 
     %x, %X  These verbs display an integer value as a hexadecimal string. The letters A–F are displayed in
             lowercase by the %x verb and in uppercase by the %X verb.
+    
+    example:
+        package main
+        import "fmt"
+        func Printfln(template string, values ...interface{}) {
+            fmt.Printf(template+"\n", values...)
+        }
+        func main() {
+            number := 250
+            Printfln("Binary: %b", number)
+            Printfln("Decimal: %d", number)
+            Printfln("Octal: %o, %O", number, number)
+            Printfln("Hexadecimal: %x, %X", number, number)
+        }
+    Output:
+        Binary: 11111010
+        Decimal: 250
+        Octal: 372, 0o372
+        Hexadecimal: fa, FA
 ████████████████████████████████████████████████████████████████████████
 213.
 ████████████████████████████████████████████████████████████████████████
