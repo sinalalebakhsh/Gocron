eadd6687c763bd0102a07fe33752725597120c6c
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-16 06:22:23 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -5328,9 +5328,25 @@ Output:
         Current Day: 16 Month: Sep Year: 2023
         Specific Day: 31 Month: May Year: 1993
         UNIX Day: 31 Month: Dec Year: 1969
-
 ████████████████████████████████████████████████████████████████████████
-249.
+249.The Layout Constants Defined by the time Package
+    Name            Reference Date Format
+    -----------     ----------------------------------
+    ANSIC           Mon Jan _2 15:04:05 2006
+    UnixDate        Mon Jan _2 15:04:05 MST 2006
+    RubyDate        Mon Jan 02 15:04:05 -0700 2006
+    RFC822          02 Jan 06 15:04 MST
+    RFC822Z         02 Jan 06 15:04 -0700
+    RFC850          Monday, 02-Jan-06 15:04:05 MST
+    RFC1123         Mon, 02 Jan 2006 15:04:05 MST
+    RFC1123Z        Mon, 02 Jan 2006 15:04:05 -0700
+    RFC3339         2006-01-02T15:04:05Z07:00
+    RFC3339Nano     2006-01-02T15:04:05.999999999Z07:00
+    Kitchen         3:04PM
+    Stamp           Jan _2 15:04:05
+    StampMilli      Jan _2 15:04:05.000
+    StampMicro      Jan _2 15:04:05.000000
+    StampNano       Jan _2 15:04:05.000000000
 ████████████████████████████████████████████████████████████████████████
 250.
 ████████████████████████████████████████████████████████████████████████
