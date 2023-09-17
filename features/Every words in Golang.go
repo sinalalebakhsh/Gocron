da02e078c50dd7f981a1efd4e413c1fba50cf4c1
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-17 18:36:11 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -6026,7 +6026,33 @@ Output:
     Read(byteSlice)     This method reads data into the specified []byte. The method returns the number of
                         bytes that were read, expressed as an int, and an error.
 ████████████████████████████████████████████████████████████████████████
-275.
+275.io.Reader package
+    example:
+        package main
+        import (
+            "io"
+            "strings"
+        )
+        func processData(reader io.Reader) {
+            b := make([]byte, 2)
+            for {
+                count, err := reader.Read(b)
+                if count > 0 {
+                    Printfln("Read %v bytes: %v", count, string(b[0:count]))
+                }
+                if err == io.EOF {
+                    break
+                }
+            }
+        }
+        func main() {
+            r := strings.NewReader("Kayak")
+            processData(r)
+        }
+    Output:
+        Read 2 bytes: Ka
+        Read 2 bytes: ya
+        Read 1 bytes: k
 ████████████████████████████████████████████████████████████████████████
 276.
 ████████████████████████████████████████████████████████████████████████
