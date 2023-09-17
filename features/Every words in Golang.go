4ff254c49da96b8ba977ea91e06dfed76ba4e701
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-17 17:39:35 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -5950,6 +5950,14 @@ Output:
         Read name: Dora
 ████████████████████████████████████████████████████████████████████████
 273.Reading and Writing Data
+    These interfaces are used wherever data is read or written, which means that any
+    source or destination for data can be treated in much the same way so that writing data to a file, for example,
+    is just the same as writing data to a network connection.
+
+    What are they?
+    These interfaces define the basic methods required to read and write data.
+
+    
 ████████████████████████████████████████████████████████████████████████
 274.
 ████████████████████████████████████████████████████████████████████████
