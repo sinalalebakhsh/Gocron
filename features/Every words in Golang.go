918030f8ce4f4ed54d1a98f025a66ecc6d8305c0
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-17 08:00:40 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -5720,7 +5720,43 @@ Output:
     Using a Timeout in a Select Statement
 
     example:
-        
+        package main
+        import (
+            "fmt"
+            "time"
+        )
+        func Printfln(template string, values ...interface{}) {
+            fmt.Printf(template+"\n", values...)
+        }
+        func writeToChannel(channel chan<- string) {
+            Printfln("Waiting for initial duration...")
+            _ = <-time.After(time.Second * 2)
+            Printfln("Initial duration elapsed.")
+            names := []string{"Alice", "Bob", "Charlie", "Dora"}
+            for _, name := range names {
+                channel <- name
+                time.Sleep(time.Second * 3)
+            }
+            close(channel)
+        }
+        func main() {
+            nameChannel := make(chan string)
+            go writeToChannel(nameChannel)
+            channelOpen := true
+            for channelOpen {
+                Printfln("Starting channel read")
+                select {
+                case name, ok := <-nameChannel:
+                    if !ok {
+                        channelOpen = false
+                    } else {
+                        Printfln("Read name: %v", name)
+                    }
+                case <-time.After(time.Second * 2):
+                    Printfln("Timeout")
+                }
+            }
+        }
     Output:
 ████████████████████████████████████████████████████████████████████████
 269.NewTimer(duration)
