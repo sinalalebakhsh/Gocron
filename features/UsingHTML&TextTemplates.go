fb644f9576c97259469fa3d13a6df47fc38a8444
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-09-22 04:54:20 -0400

    ++

--- features/UsingHTML&TextTemplates.go
+++ features/UsingHTML&TextTemplates.go
@@ -1,3 +1,20 @@
 package features
 
-var TitleOfUsingHTML
+var TitleOfUsingHTMLAndTextTemplates = []string{
+	"HTML AND TEXT TEMPLATES",
+	"HTML AND TEXT TEMPLATE",
+	"USING HTML AND TEXT TEMPLATES",
+	"USING HTML AND TEXT TEMPLATE",
+	"HTML-TEMPLATES",
+}
+
+
+type UsingHTMLTemplates struct {
+	allHTMLAndTemplates string
+}
+
+var OriginalHTMLAndTemplates = UsingHTMLTemplates {
+	allHTMLAndTemplates: `
+336.`,
+}
+
