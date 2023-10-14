58309a6d9b54adeee1bbd5b193868079b2a5a67a
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-14 11:39:59 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -11353,7 +11353,34 @@ Output:
     to the server and creates the request body by reading content from a Reader.
 
     Posting from a Reader in the main.go:
-        
+        package main
+        import (
+            "net/http"
+            "os"
+            "time"
+            "io"
+            "encoding/json"
+            "strings"
+        )
+        func main() {
+            go http.ListenAndServe(":5000", nil)
+            time.Sleep(time.Second)
+            var builder strings.Builder
+            err := json.NewEncoder(&builder).Encode(Products[0])
+            if (err == nil) {
+                response, err := http.Post("http://localhost:5000/echo",
+                    "application/json",
+                    strings.NewReader(builder.String()))
+                if (err == nil && response.StatusCode == http.StatusOK) {
+                    io.Copy(os.Stdout, response.Body)
+                    defer response.Body.Close()
+                } else {
+                    Printfln("Error: %v", err.Error())
+                }
+            } else {
+                Printfln("Error: %v", err.Error())
+            }
+        }
     ====================================================================
     Output:
 ████████████████████████████████████████████████████████████████████████
