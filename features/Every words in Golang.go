77256714c12d930e84c333c34aca3451b80ef8d3
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-16 08:38:17 -0400

    ++

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -11453,7 +11453,51 @@ Output:
     Parse(string)   This method parses a string into a URL. The results are the URL value and an error that
                     indicates problems parsing the string.
 ████████████████████████████████████████████████████████████████████████
-448.
+448.Sending a Request in the main.go
+    main.go:
+        package main
+        import (
+            "net/http"
+            "os"
+            "time"
+            "io"
+            "encoding/json"
+            "strings"
+            "net/url"
+        )
+        func main() {
+            go http.ListenAndServe(":5000", nil)
+            time.Sleep(time.Second)
+            var builder strings.Builder
+            err := json.NewEncoder(&builder).Encode(Products[0])
+            if (err == nil) {
+                reqURL, err := url.Parse("http://localhost:5000/echo")
+                if (err == nil) {
+                    req := http.Request {
+                        Method: http.MethodPost,
+                        URL: reqURL,
+                        Header: map[string][]string {
+                            "Content-Type": { "application.json" },
+                        },
+                        Body: io.NopCloser(strings.NewReader(builder.String())),
+                    }
+                    response, err := http.DefaultClient.Do(&req)
+                    if (err == nil && response.StatusCode == http.StatusOK) {
+                        io.Copy(os.Stdout, response.Body)
+                        defer response.Body.Close()
+                    } else {
+                        Printfln("Request Error: %v", err.Error())
+                    }
+                } else {
+                    Printfln("Parse Error: %v", err.Error())
+                }
+            } else {
+                Printfln("Encoder Error: %v", err.Error())
+            }
+        }
+    ====================================================================
+    Output:
+        
 ████████████████████████████████████████████████████████████████████████
 449.
 ████████████████████████████████████████████████████████████████████████
