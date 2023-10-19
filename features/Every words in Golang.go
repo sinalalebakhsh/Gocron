9f0ea234074d7b6e13e381f6150e632e5b086728
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-19 15:06:55 -0400

    Add to Every words in Golang

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -11516,10 +11516,49 @@ Output:
 ████████████████████████████████████████████████████████████████████████
 450.Using the Convenience Function in the main.go
     example:
-        
+    main.go:
+        package main
+        import (
+            "encoding/json"
+            "io"
+            "net/http"
+            "os"
+            "strings"
+            "time"
+            //"net/url"
+        )
+        func main() {
+            go http.ListenAndServe(":5000", nil)
+            time.Sleep(time.Second)
+            var builder strings.Builder
+            err := json.NewEncoder(&builder).Encode(Products[0])
+            if err == nil {
+                req, err := http.NewRequest(http.MethodPost, "http://localhost:5000/echo",
+                    io.NopCloser(strings.NewReader(builder.String())))
+                if err == nil {
+                    req.Header["Content-Type"] = []string{"application/json"}
+                    response, err := http.DefaultClient.Do(req)
+                    if err == nil && response.StatusCode == http.StatusOK {
+                        io.Copy(os.Stdout, response.Body)
+                        defer response.Body.Close()
+                    } else {
+                        Printfln("Request Error: %v", err.Error())
+                    }
+                } else {
+                    Printfln("Request Init Error: %v", err.Error())
+                }
+            } else {
+                Printfln("Encoder Error: %v", err.Error())
+            }
+        }
     ====================================================================
     Output:
-      
+        Method: POST
+        Header: User-Agent: [Go-http-client/1.1]
+        Header: Content-Type: [application/json]
+        Header: Accept-Encoding: [gzip]
+        ----
+        {"Name":"Kayak","Category":"Watersports","Price":279}
 ████████████████████████████████████████████████████████████████████████
 451.
 ████████████████████████████████████████████████████████████████████████
