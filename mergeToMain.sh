2d43f495cd79f331d8975775b54206934bac21cb
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-19 15:29:58 -0400

    ++

---
+++ mergeToMain.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+git checkout beforeMergeToMain
+git merge develop
+go build .
+echo "Go build is run"
+for i in {1..5}
+do
+    echo -n "* "
+    sleep 1
+done
+echo ""
+echo "Git is Push"
+for i in {1..5}
+do
+    echo -n "* "
+    sleep 1
+done
+echo ""
+git add .
+git commit -m " ++ "
+git push -u origin beforeMergeToMain 
+git checkout main
+git merge beforeMergeToMain
+go build .
+echo "Go build is run"
+for i in {1..5}
+do
+    echo -n "* "
+    sleep 1
+done
+echo ""
+echo "Git is Push"
+for i in {1..5}
+do
+    echo -n "* "
+    sleep 1
+done
+echo ""
+git add .
+git commit -m " ++ "
+git push -u origin main 
+echo ""
+echo -n "Please wait "
+for i in {1..3}
+do
+    echo -n ". "
+    sleep 1
+done
