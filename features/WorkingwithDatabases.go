af655fcc858ccf0c1c85c100eab3adc65669731e
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-10-29 11:32:04 -0400

    Add a feature for Question/Answer from user

---
+++ features/WorkingwithDatabases.go
@@ -0,0 +1,13 @@
+package features
+
+var TitleDataBases = []string{
+	"ALL WORKING WITH DATABASES",
+	"ALLWORKINGWITHDATABASES",
+	"ALL DATABASES",
+	"ALLDATABASES",
+}
+
+var OriginalDataBases = DataBase{
+	Alldatafield: `
+	`,
+}
