98abfc8c29538305babf6e8c583897abdd65d67f
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-10 15:11:41 -0500

    💠💠💠 Add

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -12648,7 +12648,78 @@ Output:
         Other: <main.Product Value>
 
 ████████████████████████████████████████████████████████████████████████
-486.
+486.Comparing Types in the main.go
+    main.go:
+        package main
+        import (
+            "reflect"
+            // "strings"
+            // "fmt"
+        )
+        var intPtrType = reflect.TypeOf((*int)(nil))
+        func printDetails(values ...interface{}) {
+            for _, elem := range values {
+                elemValue := reflect.ValueOf(elem)
+                elemType := reflect.TypeOf(elem)
+                if elemType == intPtrType {
+                    Printfln("Pointer to Int: %v", elemValue.Elem().Int())
+                } else {
+                    switch elemValue.Kind() {
+                    case reflect.Bool:
+                        var val bool = elemValue.Bool()
+                        Printfln("Bool: %v", val)
+                    case reflect.Int:
+                        var val int64 = elemValue.Int()
+                        Printfln("Int: %v", val)
+                    case reflect.Float32, reflect.Float64:
+                        var val float64 = elemValue.Float()
+                        Printfln("Float: %v", val)
+                    case reflect.String:
+                        var val string = elemValue.String()
+                        Printfln("String: %v", val)
+                    // case reflect.Ptr:
+                    //	 var val reflect.Value = elemValue.Elem()
+                    //	 if (val.Kind() == reflect.Int) {
+                    //		 Printfln("Pointer to Int: %v", val.Int())
+                    //	 }
+                    default:
+                        Printfln("Other: %v", elemValue.String())
+                    }
+                }
+            }
+        }
+        func main() {
+            product := Product{
+                Name: "Kayak", Category: "Watersports", Price: 279,
+            }
+            number := 100
+            printDetails(true, 10, 23.30, "Alice", &number, product)
+        }
+    ====================================================================
+    printer.go:
+        package main
+        import "fmt"
+        func Printfln(template string, values ...interface{}) {
+            fmt.Printf(template + "\n", values...)
+        }
+    ====================================================================
+    types:
+        package main
+        type Product struct {
+            Name, Category string
+            Price float64
+        }
+        type Customer struct {
+            Name, City string
+        }
+    ====================================================================
+    Output:
+        Bool: true
+        Int: 10
+        Float: 23.3
+        String: Alice
+        Pointer to Int: 100
+        Other: <main.Product Value>
 ████████████████████████████████████████████████████████████████████████
 487.
 ████████████████████████████████████████████████████████████████████████
