7a9a2bc7a270a4ee879698373087d9aa9db86bc9
sinalalebakhsh <lalebakhshsina@gmail.com>
2023-11-21 02:30:49 -0500

    💠 Add content to => 487.Identifying Byte Slices

--- features/Every words in Golang.go
+++ features/Every words in Golang.go
@@ -12722,6 +12722,78 @@ Output:
         Other: <main.Product Value>
 ████████████████████████████████████████████████████████████████████████
 487.Identifying Byte Slices
+    Identifying Byte Slices in the main.go
+    main.go:
+        package main
+        import (
+            "reflect"
+            // "strings"
+            // "fmt"
+        )
+        var intPtrType = reflect.TypeOf((*int)(nil))
+        var byteSliceType = reflect.TypeOf([]byte(nil))
+        func printDetails(values ...interface{}) {
+            for _, elem := range values {
+                elemValue := reflect.ValueOf(elem)
+                elemType := reflect.TypeOf(elem)
+                if elemType == intPtrType {
+                    Printfln("Pointer to Int: %v", elemValue.Elem().Int())
+                } else if elemType == byteSliceType {
+                    Printfln("Byte slice: %v", elemValue.Bytes())
+                } else {
+                    switch elemValue.Kind() {
+                    case reflect.Bool:
+                        var val bool = elemValue.Bool()
+                        Printfln("Bool: %v", val)
+                    case reflect.Int:
+                        var val int64 = elemValue.Int()
+                        Printfln("Int: %v", val)
+                    case reflect.Float32, reflect.Float64:
+                        var val float64 = elemValue.Float()
+                        Printfln("Float: %v", val)
+                    case reflect.String:
+                        var val string = elemValue.String()
+                        Printfln("String: %v", val)
+                    default:
+                        Printfln("Other: %v", elemValue.String())
+                    }
+                }
+            }
+        }
+        func main() {
+            product := Product{
+                Name: "Kayak", Category: "Watersports", Price: 279,
+            }
+            number := 100
+            slice := []byte("Alice")
+            printDetails(true, 10, 23.30, "Alice", &number, product, slice)
+        }
+    ==================================================================
+    printer.go:
+        package main
+        import "fmt"
+        func Printfln(template string, values ...interface{}) {
+            fmt.Printf(template + "\n", values...)
+        }
+    ==================================================================
+    types.go:
+        package main
+        type Product struct {
+            Name, Category string
+            Price float64
+        }
+        type Customer struct {
+            Name, City string
+        }
+    ==================================================================
+    Output:
+        Bool: true
+        Int: 10
+        Float: 23.3
+        String: Alice
+        Pointer to Int: 100
+        Other: <main.Product Value>
+        Byte slice: [65 108 105 99 101]
 ████████████████████████████████████████████████████████████████████████
 488.go tool dist list
     Output:
